047f1aba81ded7e40234951236a0112b74fa9791
Ali Alaa <hello@alialaa.com>
2022-02-08 16:38:36 +0200

    Folder Structure

---
+++ src/screens/settings/settings.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import styles from "./settings.styles";
+
+export default function Settings() {
+    return (
+        <View style={styles.container}>
+            <Text>Settings Page</Text>
+        </View>
+    );
+}
