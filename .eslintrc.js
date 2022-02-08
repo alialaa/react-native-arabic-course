0b56207c777564bc73659596355a53403373e8a4
Ali Alaa <hello@alialaa.com>
2022-02-08 13:09:36 +0200

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "no-console": "warn"
+    }
+};
