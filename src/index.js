4e1d7386327e5e73643f1752dbea032dd01c35f8
Ali Alaa <hello@alialaa.com>
2022-02-10 13:03:02 +0200

    Added image styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import { StyleSheet, View, Text, Platform } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <View style={styles.box}>
-                <Text style={styles.text}>Some Text</Text>
-            </View>
+            <Image style={styles.image} source={require("@assets/check.png")} />
         </View>
     );
 }
@@ -17,47 +15,14 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center"
     },
-    box: {
-        backgroundColor: Platform.OS === "android" ? "red" : "#1ca663",
-        width: "50%",
-        // height: 70,
-        padding: 10,
-        marginTop: 0,
+    image: {
+        backgroundColor: "red",
+        width: 200,
+        resizeMode: "contain",
+        tintColor: "#fff",
         borderWidth: 3,
-        borderColor: "#000",
         borderRadius: 10,
-        opacity: 1,
-        shadowColor: "#000",
-        shadowOffset: { width: 5, height: 5 },
-        shadowRadius: 5,
-        shadowOpacity: 0.6,
-        elevation: 5
-    },
-    text: {
-        // fontSize: 24,
-        // fontWeight: "600",
-        // fontStyle: "italic",
-        ...Platform.select({
-            ios: {
-                fontSize: 24,
-                fontWeight: "600",
-                fontStyle: "italic"
-            },
-            android: {
-                fontSize: 20,
-                fontWeight: "300"
-            },
-            default: {}
-        }),
-        color: "#fff",
-        letterSpacing: 1,
-        textAlign: "center",
-        textDecorationLine: "underline",
-        textDecorationStyle: "double",
-        textDecorationColor: "#000",
-        textTransform: "uppercase",
-        textShadowColor: "#000",
-        textShadowOffset: { width: 2, height: 2 },
-        textShadowRadius: 1
+        opacity: 1
+        // height: 50
     }
 });
