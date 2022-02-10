cefff99d5b6205e993a4ae230d18c8c4180dd7f1
Ali Alaa <hello@alialaa.com>
2022-02-10 14:41:48 +0200

    Flexbox demos

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Image style={styles.image} source={require("@assets/check.png")} />
+            <View style={[styles.box, { backgroundColor: "teal" }]} />
+            <View style={[styles.box, { backgroundColor: "purple" }]} />
+            <View style={[styles.box, { backgroundColor: "slateblue" }]} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "#e8e8e8",
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "center"
+        flex: 1,
+        backgroundColor: "#222",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "flex-start",
+        flexWrap: "wrap",
+        alignContent: "space-around"
     },
-    image: {
+    box: {
         backgroundColor: "red",
-        width: 200,
-        resizeMode: "contain",
-        tintColor: "#fff",
-        borderWidth: 3,
-        borderRadius: 10,
-        opacity: 1
-        // height: 50
+        width: 50,
+        height: 400
     }
 });
