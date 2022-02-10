75b06430c01802402491420aea37c60d36fc0af5
Ali Alaa <hello@alialaa.com>
2022-02-10 12:37:17 +0200

    Styling Basics

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import { StyleSheet, View } from "react-native";
-import { Home, Settings } from "@screens";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Home />
-            <Settings />
+            <View style={styles.box}>
+                <Text style={styles.text}>Some Text</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
+        backgroundColor: "#e8e8e8",
+        height: "100%"
+    },
+    box: {},
+    text: {}
 });
