bc108c77d29af1f0da2ec2022d8baccd93c877da
Ali Alaa <hello@alialaa.com>
2022-02-12 11:32:36 +0200

    Loading Fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Cairo_400Regular, Cairo_600SemiBold, Cairo_700Bold } from "@expo-google-fonts/cairo";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_500Medium,
+        Roboto_700Bold,
+        Cairo_400Regular,
+        Cairo_600SemiBold,
+        Cairo_700Bold
+    });
+    if (!fontsLoaded) return <AppLoading />;
     return (
         <View style={styles.container}>
-            <View style={[styles.box, { backgroundColor: "teal" }]} />
-            <View style={[styles.box, { backgroundColor: "purple" }]} />
-            <View style={[styles.box, { backgroundColor: "slateblue" }]} />
+            <Text style={{ color: "#fff", fontSize: 28 }}>عربي</Text>
+            <Text
+                style={{
+                    fontSize: 28,
+                    color: "#fff",
+                    fontFamily: "Cairo_700Bold"
+                }}
+            >
+                بالعربي
+            </Text>
         </View>
     );
 }
@@ -14,15 +34,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#222",
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        alignItems: "flex-start",
-        flexWrap: "wrap",
-        alignContent: "space-around"
-    },
-    box: {
-        backgroundColor: "red",
-        width: 50,
-        height: 400
+        justifyContent: "center",
+        alignItems: "center"
     }
 });
