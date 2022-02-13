fa116ba6c9e759e8284eac3fe681afc496e6de7e
Ali Alaa <hello@alialaa.com>
2022-02-13 13:15:06 +0200

    RTL & LTR switch

--- src/components/app-setup/app-setup.js
+++ src/components/app-setup/app-setup.js
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
+import { I18nManager } from "react-native";
 import PropTypes from "prop-types";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
+import { reloadAsync } from "expo-updates";
 import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Cairo_400Regular, Cairo_600SemiBold, Cairo_700Bold } from "@expo-google-fonts/cairo";
+
+I18nManager.allowRTL(true);
 
 export default function AppSetup({ children }) {
     const [fontsLoaded] = useFonts({
@@ -13,6 +18,21 @@ export default function AppSetup({ children }) {
         Cairo_600SemiBold,
         Cairo_700Bold
     });
+    useEffect(() => {
+        const loadLanguage = async () => {
+            const lang = "en";
+            const isRTL = I18nManager.isRTL;
+            if (lang === "en" && isRTL) {
+                await I18nManager.forceRTL(false);
+                await reloadAsync();
+            }
+            if (lang === "ar" && !isRTL) {
+                await I18nManager.forceRTL(true);
+                await reloadAsync();
+            }
+        };
+        loadLanguage();
+    }, []);
     return fontsLoaded ? children : <AppLoading />;
 }
 
