888b94f5ec1c001c2972678366c0c8ee86221843
Ali Alaa <hello@alialaa.com>
2022-02-28 13:12:50 +0200

    Scrollview in settings

--- src/screens/settings/settings.js
+++ src/screens/settings/settings.js
@@ -1,32 +1,9 @@
-import { View, Button } from "react-native";
+import { View, ScrollView } from "react-native";
 import { Text } from "@components";
 import { useSettings } from "@contexts";
 import styles from "./settings.styles";
 
 export default function Settings() {
     const { settings, setSettings } = useSettings();
-    return (
-        <View style={styles.container}>
-            <Text>{settings.lang}</Text>
-            <Text>{settings.units}</Text>
-            <Button
-                title="Toggle Units"
-                onPress={() => {
-                    setSettings({ units: settings.units === "metric" ? "imperial" : "metric" });
-                }}
-            />
-            <Button
-                title="English"
-                onPress={() => {
-                    setSettings({ lang: "en" });
-                }}
-            />
-            <Button
-                title="Arabic"
-                onPress={() => {
-                    setSettings({ lang: "ar" });
-                }}
-            />
-        </View>
-    );
+    return <ScrollView contentContainerStyle={styles.container}></ScrollView>;
 }
