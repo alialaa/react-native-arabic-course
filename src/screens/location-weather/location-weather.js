b3607157097e7fb8c254349d6ce2542bd690e6e3
Ali Alaa <hello@alialaa.com>
2022-03-05 12:56:27 +0200

    getting location

--- src/screens/location-weather/location-weather.js
+++ src/screens/location-weather/location-weather.js
@@ -1,17 +1,42 @@
-import { useEffect } from "react";
-import { View } from "react-native";
-import { useForegroundPermissions, Accuracy } from "expo-location";
+import { useEffect, useState } from "react";
+import { View, ActivityIndicator } from "react-native";
+import { useForegroundPermissions, Accuracy, getCurrentPositionAsync } from "expo-location";
+import { StatusBar } from "expo-status-bar";
+import { useTheme } from "@react-navigation/native";
 import styles from "./location-weather.styles";
 import NoPermissions from "./no-permissions";
 
-export default function LocationWeather({ navigation }) {
+export default function LocationWeather() {
+    const { colors } = useTheme();
+    const [location, setLocation] = useState(null);
     const [status, requestPermission] = useForegroundPermissions();
     const granted = status && status.granted;
     useEffect(() => {
         requestPermission();
     }, []);
 
+    useEffect(() => {
+        const getLocation = async () => {
+            const location = await getCurrentPositionAsync({
+                accuracy: Accuracy.Highest,
+                maximumAge: 10000
+            });
+            setLocation(location);
+        };
+        if (granted) {
+            getLocation();
+        }
+    }, [granted]);
+
     if (!granted) return <NoPermissions />;
+
+    if (location === null)
+        return (
+            <View style={styles.centeredContainer}>
+                <ActivityIndicator color={colors.primary} />
+                <StatusBar style="auto" />
+            </View>
+        );
 
     return <View style={styles.container}></View>;
 }
