cc261db667d86d4c08a13713e44c6d37313a2aba
Ali Alaa <hello@alialaa.com>
2022-03-06 15:45:30 +0200

    Styling current p1

--- src/components/full-weather-card/full-weather-card.js
+++ src/components/full-weather-card/full-weather-card.js
@@ -1,32 +1,60 @@
 import PropTypes from "prop-types";
 import { ScrollView, View, Image } from "react-native";
 import moment from "moment-timezone";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 import { getBgColor, displayTemperature, getWeatherIcon } from "@utils";
 import Text from "../text/text";
 import i18n from "@langs";
+import styles from "./full-weather-card.styles";
 
 export default function FullWeatherCard({ locationData, locationName, lang }) {
     const { current, timezone, daily } = locationData;
     const { dt, sunrise, sunset, temp, weather } = current;
     const locationDate = moment(new Date(dt * 1000)).tz(timezone);
     return (
-        <ScrollView>
-            <View style={{ height: 300, backgroundColor: getBgColor(dt, sunrise, sunset) }} />
-            <Text>{locationName || i18n.t("weather.yourLocation")}</Text>
-            <Text>{locationDate.format("dddd, D MMMM")}</Text>
-            <Text>{locationDate.format("HH:mm")}</Text>
-            <Text>{displayTemperature(temp, lang)}</Text>
-            <Image
-                style={{ width: 50, height: 50 }}
-                source={getWeatherIcon(weather[0].icon).source}
-            />
-            <Text>{weather[0].description}</Text>
-            <View style={{ flexDirection: "row" }}>
-                <Text>{displayTemperature(daily[0].temp.max, lang)}</Text>
-                <Text>/</Text>
-                <Text>{displayTemperature(daily[0].temp.min, lang)}</Text>
-            </View>
-        </ScrollView>
+        <View style={{ flex: 1 }}>
+            <SafeAreaView
+                edges={["top"]}
+                style={{ backgroundColor: getBgColor(dt, sunrise, sunset) }}
+            >
+                <View style={styles.locationNameContainer}>
+                    <Text weight="700" style={styles.locationNameText}>
+                        {locationName || i18n.t("weather.yourLocation")}
+                    </Text>
+                </View>
+            </SafeAreaView>
+
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                <View>
+                    <LinearGradient
+                        colors={[getBgColor(dt, sunrise, sunset), "transparent"]}
+                        style={styles.gradient}
+                    />
+                    <View
+                        style={[
+                            styles.scrollOffset,
+                            {
+                                backgroundColor: getBgColor(dt, sunrise, sunset)
+                            }
+                        ]}
+                    />
+                    <Text>{locationDate.format("dddd, D MMMM")}</Text>
+                    <Text>{locationDate.format("HH:mm")}</Text>
+                    <Text>{displayTemperature(temp, lang)}</Text>
+                    <Image
+                        style={{ width: 50, height: 50 }}
+                        source={getWeatherIcon(weather[0].icon).source}
+                    />
+                    <Text>{weather[0].description}</Text>
+                    <View style={{ flexDirection: "row" }}>
+                        <Text>{displayTemperature(daily[0].temp.max, lang)}</Text>
+                        <Text>/</Text>
+                        <Text>{displayTemperature(daily[0].temp.min, lang)}</Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </View>
     );
 }
 
--- src/components/full-weather-card/full-weather-card.styles.js
+++ src/components/full-weather-card/full-weather-card.styles.js
@@ -1,5 +1,28 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    locationNameContainer: {
+        alignItems: "center",
+        padding: 10
+    },
+    locationNameText: {
+        color: "#fff",
+        fontSize: 28,
+        textShadowColor: "rgba(1,1,1,0.5)",
+        textShadowRadius: 1,
+        textShadowOffset: { width: 1, height: 1 }
+    },
+    gradient: {
+        position: "absolute",
+        width: "100%",
+        height: "400%"
+    },
+    scrollOffset: {
+        position: "absolute",
+        width: "100%",
+        height: 1000,
+        marginTop: -1000
+    }
+});
 
 export default styles;
