7f2b62aac41b2a82f839a260b769770f39e49722
Ali Alaa <hello@alialaa.com>
2022-03-06 16:10:13 +0200

    styling current cont.

--- src/components/full-weather-card/full-weather-card.js
+++ src/components/full-weather-card/full-weather-card.js
@@ -12,6 +12,7 @@ export default function FullWeatherCard({ locationData, locationName, lang }) {
     const { current, timezone, daily } = locationData;
     const { dt, sunrise, sunset, temp, weather } = current;
     const locationDate = moment(new Date(dt * 1000)).tz(timezone);
+    const weatherIcon = getWeatherIcon(weather[0].icon);
     return (
         <View style={{ flex: 1 }}>
             <SafeAreaView
@@ -19,7 +20,7 @@ export default function FullWeatherCard({ locationData, locationName, lang }) {
                 style={{ backgroundColor: getBgColor(dt, sunrise, sunset) }}
             >
                 <View style={styles.locationNameContainer}>
-                    <Text weight="700" style={styles.locationNameText}>
+                    <Text weight="700" style={[styles.currentWeatherText, styles.locationNameText]}>
                         {locationName || i18n.t("weather.yourLocation")}
                     </Text>
                 </View>
@@ -39,18 +40,45 @@ export default function FullWeatherCard({ locationData, locationName, lang }) {
                             }
                         ]}
                     />
-                    <Text>{locationDate.format("dddd, D MMMM")}</Text>
-                    <Text>{locationDate.format("HH:mm")}</Text>
-                    <Text>{displayTemperature(temp, lang)}</Text>
-                    <Image
-                        style={{ width: 50, height: 50 }}
-                        source={getWeatherIcon(weather[0].icon).source}
-                    />
-                    <Text>{weather[0].description}</Text>
-                    <View style={{ flexDirection: "row" }}>
-                        <Text>{displayTemperature(daily[0].temp.max, lang)}</Text>
-                        <Text>/</Text>
-                        <Text>{displayTemperature(daily[0].temp.min, lang)}</Text>
+                    <View style={styles.currentWeatherContainer}>
+                        <Text style={styles.currentWeatherText}>
+                            {locationDate.format("dddd, D MMMM")}
+                        </Text>
+                        <Text style={[styles.currentWeatherText, { marginTop: 10 }]}>
+                            {locationDate.format("HH:mm")}
+                        </Text>
+
+                        <View style={styles.tempIconContainer}>
+                            <Image
+                                style={[
+                                    styles.weatherIcon,
+                                    {
+                                        width: 50 * (weatherIcon.width / weatherIcon.height),
+                                        height: 50
+                                    }
+                                ]}
+                                source={weatherIcon.source}
+                            />
+                            <Text
+                                weight="700"
+                                style={[styles.currentWeatherText, { fontSize: 34 }]}
+                            >
+                                {displayTemperature(temp, lang)}
+                            </Text>
+                        </View>
+
+                        <Text style={[styles.currentWeatherText, { marginBottom: 5 }]}>
+                            {weather[0].description}
+                        </Text>
+                        <View style={{ flexDirection: "row" }}>
+                            <Text style={styles.currentWeatherText}>
+                                {displayTemperature(daily[0].temp.max, lang)}
+                            </Text>
+                            <Text style={styles.currentWeatherText}> / </Text>
+                            <Text style={styles.currentWeatherText}>
+                                {displayTemperature(daily[0].temp.min, lang)}
+                            </Text>
+                        </View>
                     </View>
                 </View>
             </ScrollView>
--- src/components/full-weather-card/full-weather-card.styles.js
+++ src/components/full-weather-card/full-weather-card.styles.js
@@ -3,14 +3,18 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     locationNameContainer: {
         alignItems: "center",
-        padding: 10
+        padding: 10,
+        paddingTop: 20
     },
-    locationNameText: {
+    currentWeatherText: {
         color: "#fff",
-        fontSize: 28,
+        fontSize: 18,
         textShadowColor: "rgba(1,1,1,0.5)",
         textShadowRadius: 1,
         textShadowOffset: { width: 1, height: 1 }
+    },
+    locationNameText: {
+        fontSize: 28
     },
     gradient: {
         position: "absolute",
@@ -22,6 +26,21 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 1000,
         marginTop: -1000
+    },
+    currentWeatherContainer: {
+        alignItems: "center",
+        padding: 10
+    },
+    tempIconContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 30,
+        marginBottom: 15
+    },
+    weatherIcon: {
+        tintColor: "#fff",
+        marginRight: 10,
+        resizeMode: "contain"
     }
 });
 
