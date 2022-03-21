31313293fddec4df42a9604d98695331aef06eab
Ali Alaa <hello@alialaa.com>
2022-03-21 14:50:43 +0200

    Search screen header

--- src/config/navigator/favorites-stack.js
+++ src/config/navigator/favorites-stack.js
@@ -1,9 +1,13 @@
+import { TouchableOpacity, View, Dimensions, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text } from "@components";
 import { SearchLocations, FavoriteLocations, LocationWeather } from "@screens";
 import i18n from "@langs";
-import { commonScreenOptions } from "./navigator.styles";
+import { commonScreenOptions, DarkTheme } from "./navigator.styles";
 
 const FavoritesStack = createNativeStackNavigator();
+const screenWidth = Dimensions.get("screen").width;
 
 export default function FavoritesStackNavigator() {
     return (
@@ -11,11 +15,45 @@ export default function FavoritesStackNavigator() {
             <FavoritesStack.Screen
                 name="FavoriteLocations"
                 component={FavoriteLocations}
+                options={({ navigation }) => ({
+                    title: i18n.t("favorites.title"),
+                    headerRight: () => (
+                        <TouchableOpacity onPress={() => navigation.navigate("SearchLocations")}>
+                            <Ionicons name="search" size={20} color={DarkTheme.colors.text} />
+                        </TouchableOpacity>
+                    )
+                })}
+            />
+            <FavoritesStack.Screen
+                name="SearchLocations"
+                component={SearchLocations}
                 options={{
-                    title: i18n.t("favorites.title")
+                    animation: "fade_from_bottom",
+                    headerBackVisible: false,
+                    headerTitle: () => (
+                        <View
+                            style={{
+                                width: screenWidth - 32,
+                                height: 30,
+                                flexDirection: "row",
+                                alignItems: "center"
+                            }}
+                        >
+                            <TextInput
+                                style={{
+                                    flex: 1,
+                                    height: 30,
+                                    marginRight: 10,
+                                    backgroundColor: "red"
+                                }}
+                            />
+                            <TouchableOpacity>
+                                <Text style={{ color: DarkTheme.colors.text }}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )
                 }}
             />
-            <FavoritesStack.Screen name="SearchLocations" component={SearchLocations} />
             <FavoritesStack.Screen name="LocationWeather" component={LocationWeather} />
         </FavoritesStack.Navigator>
     );
